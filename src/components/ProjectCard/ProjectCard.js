ff1d2bcf342b3375bd6d841f8a2ef4932d0cd747
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2022-07-03 20:08:20 -0600

    Create initial ProjectCard component

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './ProjectCard.css';
 
-const ProjectCard = (props) => {
+const ProjectCard = ({project}) => {
   return (
-    <>
-    </>
+    <Link className='project-link' to={`/projects/${project.id}`}>
+      <article className='project-card'>
+        <img className='project-image' src={project.imageUrl} alt={project.title}/>
+          <h2>{project.title}</h2>
+      </article>
+    </Link>
   )
 }
 
