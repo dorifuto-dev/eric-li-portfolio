8c0d9e40558ee8ff61eddb5c19a7d472361a1331
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2022-07-04 13:26:32 -0600

    Install react-hamburger-menu, fix margins, add name text

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import './NavBar.css';
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
     <nav className='nav-bar'>
+      {location.pathname !== '/' && <h2 className='eric'>Eric Li</h2>}
       <div className='nav-link-container'>
         <Link className='link' to='/'>Home</Link>
         <Link className='link' to='/portfolio'>Portfolio</Link>
         <Link className='link' to='/resume'>Resume</Link>
         <Link className='link' to='/contact'>Contact</Link>
       </div>
+      {/*<Menu width={ '100%' } className='hamburger-nav'>*/}
+      {/*  <Link className='link' to='/'>Home</Link>*/}
+      {/*  <Link className='link' to='/portfolio'>Portfolio</Link>*/}
+      {/*  <Link className='link' to='/resume'>Resume</Link>*/}
+      {/*  <Link className='link' to='/contact'>Contact</Link>*/}
+      {/*</Menu>*/}
     </nav>
   )
 }
