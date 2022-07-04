6f3913cc08f131e60489f2f133150bb3edd7a805
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2022-07-04 02:32:13 -0600

    Create and style ProjectDetails

--- src/components/ProjectDetails/ProjectDetails.js
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link, useParams} from 'react-router-dom';
 import projects from '../../data/projectData';
-import './ProjectDetails.css';
+import './ProjectDetails.sass';
 
 const ProjectDetails = () => {
   let { id } = useParams()
@@ -9,9 +9,26 @@ const ProjectDetails = () => {
   const currentProject = projects.find(project => project.id === id)
 
   return (
-    <article>
-      <p>{currentProject.description}</p>
-      <Link to={-1}>Back</Link>
+    <article className='project-details-container'>
+      <img
+        className='project-details-image'
+        alt={currentProject.title}
+        src={currentProject.imageUrl}
+      />
+      <p className='project-details-title'>{currentProject.title}</p>
+      <div className='project-detail-links'>
+        {currentProject.deployedUrl
+          &&
+            <button class='btn btn-primary'>
+              <a className='deployed-url' href={currentProject.deployedUrl}>Deployed Link</a>
+            </button>
+        }
+        <button class='btn btn-success'>
+          <a className='github-url' href={currentProject.url}>Codebase</a>
+        </button>
+      </div>
+      <p className='project-details-description'>{currentProject.description}</p>
+      <Link className='back-link' to={-1}>Back</Link>
     </article>
   )
 }
