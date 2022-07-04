2b69a2d189b7eed49379a772a0bde702aee74f79
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2022-07-04 09:45:06 -0600

    Fix margins

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,25 +11,27 @@ const Home = () => {
   return (
     <section className='home-container'>
       <h2 className='home-title'>Eric Li</h2>
-      <div className='skill-badges-container'>
-        <img className='skill-badge' src={jsLogo} alt='javascript badge' />
-        <img className='skill-badge angular' src={ngLogo} alt='angular badge' />
-        <img className='skill-badge' src={reactLogo} alt='react badge' />
-        <img className='skill-badge' src={tsLogo} alt='typescript badge' />
-      </div>
-      <p className='home-description'>Thanks for stopping by! I am a Frontend Developer currently based out of Cottonwood Heights, Utah, United States.</p>
-      <p className='home-skillset'>My skillset is Modern Angular, React, Javascript, TypeScript, HTML, CSS. Object Oriented Programming, Test Driven Development.</p>
-      <div className='home-btns-container'>
-        <Link className="box-1" to='/portfolio'>
-          <div className="btn btn-one">
-            <span>My work</span>
-          </div>
-        </Link>
-        <Link className="box-1" to='/contact'>
-          <div className="btn btn-one">
-            <span>Get in touch</span>
-          </div>
-        </Link>
+      <div className='home-info-container'>
+        <div className='skill-badges-container'>
+          <img className='skill-badge' src={jsLogo} alt='javascript badge' />
+          <img className='skill-badge angular' src={ngLogo} alt='angular badge' />
+          <img className='skill-badge' src={reactLogo} alt='react badge' />
+          <img className='skill-badge' src={tsLogo} alt='typescript badge' />
+        </div>
+        <p className='home-description'>Thanks for stopping by! I am a Frontend Developer currently based out of Cottonwood Heights, Utah, United States.</p>
+        <p className='home-skillset'>My skillset is Modern Angular, React, Javascript, TypeScript, HTML, CSS. Object Oriented Programming, Test Driven Development.</p>
+        <div className='home-btns-container'>
+          <Link className="box-1" to='/portfolio'>
+            <div className="btn btn-one">
+              <span>My work</span>
+            </div>
+          </Link>
+          <Link className="box-1" to='/contact'>
+            <div className="btn btn-one">
+              <span>Get in touch</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </section>
   )
