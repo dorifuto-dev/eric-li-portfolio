f3b513d0b30e5d035755fe48c13278d67abbf2a2
Eric Li <75854628+dorifuto-dev@users.noreply.github.com>
2022-07-03 20:13:20 -0600

    Create inital routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Portfolio from '../Portfolio/Portfolio';
 import NavBar from '../NavBar/NavBar';
 import './App.css';
 import Contact from "../Contact/Contact";
+import ProjectDetails from "../ProjectDetails/ProjectDetails";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/portfolio' element={<Portfolio />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='/projects/:id' element={<ProjectDetails />} />
       </Routes>
     </div>
   );
